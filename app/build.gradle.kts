c0c3801baf6edc3ae3a54bb015fa0dbabdf46f0b
Ali688273 <iiii688273@gmail.com>
2026-09-13 12:06:20 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer:1.11.0")
     implementation("androidx.media3:media3-ui:1.11.0")
     implementation("androidx.media3:media3-session:1.11.0")
+    implementation("androidx.media3:media3-effect:1.11.0")
 }
