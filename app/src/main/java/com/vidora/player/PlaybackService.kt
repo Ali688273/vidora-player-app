5d007f31c0175579e44ea19bb1c38fa6e202128d
Ali688273 <iiii688273@gmail.com>
2026-09-14 07:36:29 +0330

    Create PlaybackService.kt

---
+++ app/src/main/java/com/vidora/player/PlaybackService.kt
@@ -0,0 +1,75 @@
+package com.vidora.player
+
+import android.content.Intent
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
+
+@UnstableApi
+class PlaybackService : MediaSessionService() {
+
+    private var mediaSession: MediaSession? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val audioAttributes =
+            AudioAttributes.Builder()
+                .setUsage(C.USAGE_MEDIA)
+                .setContentType(C.AUDIO_CONTENT_TYPE_MOVIE)
+                .build()
+
+        val player =
+            ExoPlayer.Builder(this)
+                .setAudioAttributes(
+                    audioAttributes,
+                    true
+                )
+                .setHandleAudioBecomingNoisy(true)
+                .build()
+
+        mediaSession =
+            MediaSession.Builder(
+                this,
+                player
+            )
+                .setId("VidoraPlayerSession")
+                .build()
+    }
+
+    override fun onGetSession(
+        controllerInfo: MediaSession.ControllerInfo
+    ): MediaSession? {
+        return mediaSession
+    }
+
+    override fun onTaskRemoved(
+        rootIntent: Intent?
+    ) {
+        /*
+         * عمداً سرویس را متوقف نمی‌کنیم.
+         *
+         * در نتیجه اگر کاربر برنامه را از صفحه برنامه‌های اخیر
+         * کنار بزند، در صورتی که پخش در حال انجام باشد،
+         * پخش می‌تواند ادامه پیدا کند.
+         */
+        super.onTaskRemoved(rootIntent)
+    }
+
+    override fun onDestroy() {
+
+        mediaSession?.run {
+
+            player.release()
+
+            release()
+        }
+
+        mediaSession = null
+
+        super.onDestroy()
+    }
+}
