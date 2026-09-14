9f0c69a86d20db7c8a1adf2c74058f5f4b7fcfa3
Ali688273 <iiii688273@gmail.com>
2026-09-14 23:01:59 +0330

    Update TapsellAdShowListener.java

--- app/src/main/java/com/vidora/player/TapsellAdShowListener.java
+++ app/src/main/java/com/vidora/player/TapsellAdShowListener.java
@@ -1,55 +1,55 @@
 package com.vidora.player;
 
-import android.util.Log;
-
 import ir.tapsell.plus.AdShowListener;
 import ir.tapsell.plus.model.TapsellPlusAdModel;
 import ir.tapsell.plus.model.TapsellPlusErrorModel;
 
 public final class TapsellAdShowListener extends AdShowListener {
 
-    public interface Callback {
-        void onOpened(TapsellPlusAdModel adModel);
+    private final Runnable onOpened;
+    private final Runnable onClosed;
+    private final Runnable onRewarded;
+    private final Runnable onError;
 
-        void onClosed(TapsellPlusAdModel adModel);
-
-        void onRewarded(TapsellPlusAdModel adModel);
-
-        void onError(TapsellPlusErrorModel error);
-    }
-
-    private final Callback callback;
-
-    public TapsellAdShowListener(Callback callback) {
+    public TapsellAdShowListener(
+            Runnable onOpened,
+            Runnable onClosed,
+            Runnable onRewarded,
+            Runnable onError
+    ) {
         super();
-        this.callback = callback;
+
+        this.onOpened = onOpened;
+        this.onClosed = onClosed;
+        this.onRewarded = onRewarded;
+        this.onError = onError;
     }
 
     @Override
     public void onOpened(TapsellPlusAdModel adModel) {
-        if (callback != null) {
-            callback.onOpened(adModel);
+        if (onOpened != null) {
+            onOpened.run();
         }
     }
 
     @Override
     public void onClosed(TapsellPlusAdModel adModel) {
-        if (callback != null) {
-            callback.onClosed(adModel);
+        if (onClosed != null) {
+            onClosed.run();
         }
     }
 
     @Override
     public void onRewarded(TapsellPlusAdModel adModel) {
-        if (callback != null) {
-            callback.onRewarded(adModel);
+        if (onRewarded != null) {
+            onRewarded.run();
         }
     }
 
     @Override
     public void onError(TapsellPlusErrorModel error) {
-        if (callback != null) {
-            callback.onError(error);
+        if (onError != null) {
+            onError.run();
         }
     }
 }
