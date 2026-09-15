f5fd0e76d897e93dba7f01fe789bd58f6ae2c1b9
Ali688273 <iiii688273@gmail.com>
2026-09-15 11:28:58 +0330

    Update PlaybackService.kt

--- app/src/main/java/com/vidora/player/PlaybackService.kt
+++ app/src/main/java/com/vidora/player/PlaybackService.kt
@@ -1,17 +1,13 @@
 package com.vidora.player
 
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
-import androidx.core.content.ContextCompat
 import androidx.media3.cast.CastPlayer
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
-import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.audio.AudioSink
 import androidx.media3.exoplayer.audio.DefaultAudioSink
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
@@ -19,108 +15,21 @@ import androidx.media3.session.MediaSessionService
 @UnstableApi
 class PlaybackService : MediaSessionService() {
 
-    private var mediaSession: MediaSession? =
-        null
+    private var mediaSession: MediaSession? = null
 
-    private var localPlayer: ExoPlayer? =
-        null
+    private var localPlayer: ExoPlayer? = null
 
-    private lateinit var audioSyncProcessor:
-        AudioSyncProcessor
-
-    private val syncReceiver =
-        object : BroadcastReceiver() {
-
-            override fun onReceive(
-                context: Context?,
-                intent: Intent?
-            ) {
-
-                if (
-                    intent?.action !=
-                    AudioSyncManager.actionChanged()
-                ) {
-                    return
-                }
-
-                val uriString =
-                    intent.getStringExtra(
-                        "uri"
-                    ) ?: return
-
-                val offset =
-                    intent.getLongExtra(
-                        "offset",
-                        0L
-                    )
-
-                val currentUri =
-                    localPlayer
-                        ?.currentMediaItem
-                        ?.localConfiguration
-                        ?.uri
-                        ?.toString()
-
-                if (
-                    currentUri ==
-                    uriString
-                ) {
-                    audioSyncProcessor
-                        .setOffset(
-                            offset
-                        )
-                }
-            }
-        }
-
-    private val playerListener =
-        object : Player.Listener {
-
-            override fun onMediaItemTransition(
-                mediaItem: androidx.media3.common.MediaItem?,
-                reason: Int
-            ) {
-
-                val uri =
-                    mediaItem
-                        ?.localConfiguration
-                        ?.uri
-                        ?: return
-
-                val offset =
-                    AudioSyncManager.getOffset(
-                        this@PlaybackService,
-                        uri
-                    )
-
-                audioSyncProcessor
-                    .setOffset(
-                        offset
-                    )
-            }
-        }
+    private var audioSyncProcessor:
+        AudioSyncProcessor? = null
 
     override fun onCreate() {
-
         super.onCreate()
 
-        audioSyncProcessor =
+        val processor =
             AudioSyncProcessor()
 
-        val audioSink =
-            DefaultAudioSink.Builder(this)
-                .setAudioProcessors(
-                    arrayOf(
-                        audioSyncProcessor
-                    )
-                )
-                .build()
-
-        val renderersFactory =
-            DefaultRenderersFactory(this)
-                .setAudioSink(
-                    audioSink
-                )
+        audioSyncProcessor =
+            processor
 
         val audioAttributes =
             AudioAttributes.Builder()
@@ -132,7 +41,42 @@ class PlaybackService : MediaSessionService() {
                 )
                 .build()
 
-        localPlayer =
+        val renderersFactory =
+            object : DefaultRenderersFactory(this) {
+
+                override fun buildAudioSink(
+                    context: android.content.Context,
+                    enableFloatOutput: Boolean,
+                    enableAudioTrackPlaybackParams: Boolean,
+                    enableOffload: Boolean
+                ): AudioSink? {
+
+                    return DefaultAudioSink.Builder(
+                        context
+                    )
+                        .setEnableFloatOutput(
+                            enableFloatOutput
+                        )
+                        .setEnableAudioTrackPlaybackParams(
+                            enableAudioTrackPlaybackParams
+                        )
+                        .setOffloadMode(
+                            if (enableOffload) {
+                                DefaultAudioSink
+                                    .OFFLOAD_MODE_ENABLED_GAPLESS_REQUIRED
+                            } else {
+                                DefaultAudioSink
+                                    .OFFLOAD_MODE_DISABLED
+                            }
+                        )
+                        .setAudioProcessors(
+                            arrayOf(processor)
+                        )
+                        .build()
+                }
+            }
+
+        val player =
             ExoPlayer.Builder(
                 this,
                 renderersFactory
@@ -146,16 +90,39 @@ class PlaybackService : MediaSessionService() {
                 )
                 .build()
 
-        localPlayer?.addListener(
-            playerListener
-        )
+        localPlayer =
+            player
 
         val castPlayer =
             CastPlayer.Builder(this)
-                .setLocalPlayer(
-                    localPlayer!!
-                )
+                .setLocalPlayer(player)
                 .build()
+
+        castPlayer.addListener(
+            object :
+                androidx.media3.common.Player.Listener {
+
+                override fun onMediaItemTransition(
+                    mediaItem:
+                        androidx.media3.common.MediaItem?,
+                    reason: Int
+                ) {
+
+                    val uri =
+                        mediaItem
+                            ?.localConfiguration
+                            ?.uri
+                            ?: return
+
+                    processor.setOffset(
+                        AudioSyncManager.getOffset(
+                            this@PlaybackService,
+                            uri
+                        )
+                    )
+                }
+            }
+        )
 
         mediaSession =
             MediaSession.Builder(
@@ -166,15 +133,6 @@ class PlaybackService : MediaSessionService() {
                     "VidoraPlayerSession"
                 )
                 .build()
-
-        ContextCompat.registerReceiver(
-            this,
-            syncReceiver,
-            IntentFilter(
-                AudioSyncManager.actionChanged()
-            ),
-            ContextCompat.RECEIVER_NOT_EXPORTED
-        )
     }
 
     override fun onGetSession(
@@ -188,6 +146,7 @@ class PlaybackService : MediaSessionService() {
     override fun onTaskRemoved(
         rootIntent: Intent?
     ) {
+
         super.onTaskRemoved(
             rootIntent
         )
@@ -195,24 +154,18 @@ class PlaybackService : MediaSessionService() {
 
     override fun onDestroy() {
 
-        try {
-            unregisterReceiver(
-                syncReceiver
-            )
-        } catch (_: Exception) {
-        }
-
-        localPlayer?.removeListener(
-            playerListener
-        )
-
         mediaSession?.run {
+
             player.release()
+
             release()
         }
 
-        localPlayer = null
         mediaSession = null
+
+        localPlayer = null
+
+        audioSyncProcessor = null
 
         super.onDestroy()
     }
