5fea7cff30f217086b9e734035f8ccbe80ed50ee
Ali688273 <iiii688273@gmail.com>
2026-09-18 14:29:13 +0330

    Update MainActivity.kt

--- app/src/main/java/com/vidora/player/MainActivity.kt
+++ app/src/main/java/com/vidora/player/MainActivity.kt
@@ -357,7 +357,7 @@ class MainActivity : ComponentActivity() {
                         this,
                         uri
                     ) &&
-                    !VidoraSettings.isShowHidden(
+                    !VidoraSettings.showHidden(
                         this
                     )
                 ) {
