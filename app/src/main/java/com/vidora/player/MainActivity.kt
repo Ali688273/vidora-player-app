22f178b01092946f79db0d8dab5af044e1b21732
Ali688273 <iiii688273@gmail.com>
2026-09-18 14:33:39 +0330

    Update MainActivity.kt

--- app/src/main/java/com/vidora/player/MainActivity.kt
+++ app/src/main/java/com/vidora/player/MainActivity.kt
@@ -235,7 +235,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
+        permissions: Array<String>,
         grantResults: IntArray
     ) {
 
