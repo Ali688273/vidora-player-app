2a691099424c435128ff47cd686bc4b749328c22
Ali688273 <iiii688273@gmail.com>
2026-09-18 21:52:16 +0330

    Update VidoraSettingsController.kt

--- app/src/main/java/com/vidora/player/VidoraSettingsController.kt
+++ app/src/main/java/com/vidora/player/VidoraSettingsController.kt
@@ -4,44 +4,137 @@ import android.content.Context
 
 object VidoraSettingsController {
 
-    fun setLanguageAuto(
-        context: Context
-    ) {
-        VidoraLanguageManager.setLanguage(
-            context,
-            VidoraLanguageManager.AUTO
-        )
-    }
-
-    fun setLanguagePersian(
-        context: Context
-    ) {
-        VidoraLanguageManager.setLanguage(
-            context,
-            VidoraLanguageManager.FA
-        )
-    }
-
-    fun setLanguageEnglish(
-        context: Context
-    ) {
-        VidoraLanguageManager.setLanguage(
-            context,
-            VidoraLanguageManager.EN
-        )
-    }
-
-    fun selectedLanguage(
+    fun getLanguage(
         context: Context
     ): String {
-        return VidoraLanguageManager
-            .getSelectedLanguage(context)
+
+        return VidoraLanguageManager.getLanguage(
+            context
+        )
     }
 
-    fun effectiveLanguage(
+    fun getSelectedLanguage(
         context: Context
     ): String {
-        return VidoraLanguageManager
-            .getEffectiveLanguage(context)
+
+        return VidoraLanguageManager.getSelectedLanguage(
+            context
+        )
+    }
+
+    fun setLanguage(
+        context: Context,
+        language: String
+    ) {
+
+        VidoraLanguageManager.setLanguage(
+            context,
+            language
+        )
+    }
+
+    fun useAutomaticLanguage(
+        context: Context
+    ) {
+
+        VidoraLanguageManager.useAutomaticLanguage(
+            context
+        )
+    }
+
+    fun isPersian(
+        context: Context
+    ): Boolean {
+
+        return VidoraLanguageManager.isPersian(
+            context
+        )
+    }
+
+    fun isEnglish(
+        context: Context
+    ): Boolean {
+
+        return VidoraLanguageManager.isEnglish(
+            context
+        )
+    }
+
+    fun isAutomatic(
+        context: Context
+    ): Boolean {
+
+        return VidoraLanguageManager.isAutomatic(
+            context
+        )
+    }
+
+    fun languageName(
+        context: Context
+    ): String {
+
+        return VidoraLanguageManager.languageName(
+            context
+        )
+    }
+
+    fun isDarkMode(
+        context: Context
+    ): Boolean {
+
+        return VidoraSettings.isDarkMode(
+            context
+        )
+    }
+
+    fun setDarkMode(
+        context: Context,
+        value: Boolean
+    ) {
+
+        VidoraSettings.setDarkMode(
+            context,
+            value
+        )
+    }
+
+    fun showHidden(
+        context: Context
+    ): Boolean {
+
+        return VidoraSettings.showHidden(
+            context
+        )
+    }
+
+    fun setShowHidden(
+        context: Context,
+        value: Boolean
+    ) {
+
+        VidoraSettings.setShowHidden(
+            context,
+            value
+        )
+    }
+
+    fun autoResume(
+        context: Context
+    ): Boolean {
+
+        return VidoraSettings.autoResume(
+            context
+        )
+    }
+
+    fun setAutoResume(
+        context: Context,
+        value: Boolean
+    ) {
+
+        VidoraSettings.setAutoResume(
+            context,
+            value
+        )
     }
 }
