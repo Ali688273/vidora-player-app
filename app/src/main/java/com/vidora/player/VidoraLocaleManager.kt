6dac382605ac992eda970fcbd24ee53453a9986a
Ali688273 <iiii688273@gmail.com>
2026-09-19 15:12:52 +0330

    Update VidoraLocaleManager.kt

--- app/src/main/java/com/vidora/player/VidoraLocaleManager.kt
+++ app/src/main/java/com/vidora/player/VidoraLocaleManager.kt
@@ -2,6 +2,8 @@ package com.vidora.player
 
 import android.content.Context
 import android.content.res.Configuration
+import android.os.Build
+import android.view.View
 import java.util.Locale
 
 object VidoraLocaleManager {
@@ -27,6 +29,10 @@ object VidoraLocaleManager {
 
         configuration.setLocale(locale)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            configuration.setLayoutDirection(locale)
+        }
+
         return context.createConfigurationContext(
             configuration
         )
@@ -48,5 +54,40 @@ object VidoraLocaleManager {
         Locale.setDefault(locale)
 
         configuration.setLocale(locale)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            configuration.setLayoutDirection(locale)
+        }
+    }
+
+    fun applyDirection(
+        view: View,
+        context: Context
+    ) {
+
+        if (
+            VidoraLanguageManager.isPersian(
+                context
+            )
+        ) {
+
+            if (
+                Build.VERSION.SDK_INT >=
+                Build.VERSION_CODES.JELLY_BEAN_MR1
+            ) {
+                view.layoutDirection =
+                    View.LAYOUT_DIRECTION_RTL
+            }
+
+        } else {
+
+            if (
+                Build.VERSION.SDK_INT >=
+                Build.VERSION_CODES.JELLY_BEAN_MR1
+            ) {
+                view.layoutDirection =
+                    View.LAYOUT_DIRECTION_LTR
+            }
+        }
     }
 }
