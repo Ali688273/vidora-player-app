ce242d2515263f1102a6dbf5ca22e0f3f45686b4
Ali688273 <iiii688273@gmail.com>
2026-09-19 14:03:13 +0330

    Update PlayerActivity.kt

--- app/src/main/java/com/vidora/player/PlayerActivity.kt
+++ app/src/main/java/com/vidora/player/PlayerActivity.kt
@@ -10,8 +10,6 @@ import android.content.pm.PackageManager
 import android.media.AudioManager
 import android.net.Uri
 import android.os.Build
-import android.view.Gravity
-import android.widget.FrameLayout
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -25,6 +23,7 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.SeekBar
 import android.widget.TextView
