3d8b5b99c183b2f0eaf1093adf7bbd84d69c1f2f
Ali688273 <iiii688273@gmail.com>
2026-09-21 07:52:56 +0330

    Restore standard Media3 audio output path to fix silent video audio

--- app/src/main/java/com/vidora/player/PlaybackService.kt
+++ app/src/main/java/com/vidora/player/PlaybackService.kt
@@ -5,10 +5,7 @@ import androidx.media3.cast.CastPlayer
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.common.util.UnstableApi
-import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.audio.AudioSink
-import androidx.media3.exoplayer.audio.DefaultAudioSink
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 
@@ -19,17 +16,8 @@ class PlaybackService : MediaSessionService() {
 
     private var localPlayer: ExoPlayer? = null
 
-    private var audioSyncProcessor:
-        AudioSyncProcessor? = null
-
     override fun onCreate() {
         super.onCreate()
-
-        val processor =
-            AudioSyncProcessor()
-
-        audioSyncProcessor =
-            processor
 
         val audioAttributes =
             AudioAttributes.Builder()
@@ -41,36 +29,8 @@ class PlaybackService : MediaSessionService() {
                 )
                 .build()
 
-        val renderersFactory =
-            object : DefaultRenderersFactory(this) {
-
-                override fun buildAudioSink(
-                    context: android.content.Context,
-                    enableFloatOutput: Boolean,
-                    enableAudioTrackPlaybackParams: Boolean
-                ): AudioSink? {
-
-                    return DefaultAudioSink.Builder(
-                        context
-                    )
-                        .setEnableFloatOutput(
-                            enableFloatOutput
-                        )
-                        .setEnableAudioTrackPlaybackParams(
-                            enableAudioTrackPlaybackParams
-                        )
-                        .setAudioProcessors(
-                            arrayOf(processor)
-                        )
-                        .build()
-                }
-            }
-
         val player =
-            ExoPlayer.Builder(
-                this,
-                renderersFactory
-            )
+            ExoPlayer.Builder(this)
                 .setAudioAttributes(
                     audioAttributes,
                     true
@@ -87,32 +47,6 @@ class PlaybackService : MediaSessionService() {
             CastPlayer.Builder(this)
                 .setLocalPlayer(player)
                 .build()
-
-        castPlayer.addListener(
-            object :
-                androidx.media3.common.Player.Listener {
-
-                override fun onMediaItemTransition(
-                    mediaItem:
-                        androidx.media3.common.MediaItem?,
-                    reason: Int
-                ) {
-
-                    val uri =
-                        mediaItem
-                            ?.localConfiguration
-                            ?.uri
-                            ?: return
-
-                    processor.setOffset(
-                        AudioSyncManager.getOffset(
-                            this@PlaybackService,
-                            uri
-                        )
-                    )
-                }
-            }
-        )
 
         mediaSession =
             MediaSession.Builder(
@@ -154,8 +88,6 @@ class PlaybackService : MediaSessionService() {
         mediaSession = null
 
         localPlayer = null
-
-        audioSyncProcessor = null
 
         super.onDestroy()
     }
